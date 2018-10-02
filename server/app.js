730374b37fd86d778a830e35bc4c13471f006d50
Shiyu Wang <shiyu3169@gmail.com>
2018-10-02 15:39:13 -0400

    start over

--- server/app.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-	require("./models/models.server.js");
-	require("./services/user.service.server.js")(app);
-	require("./services/website.service.server.js")(app);
-    require("./services/page.service.server.js")(app);
-    require("./services/widget.service.server.js")(app);
-}
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Login from "./components/user/Login";
-import "./style.css";
-
-ReactDOM.render(<Login />, document.getElementById("root"));
