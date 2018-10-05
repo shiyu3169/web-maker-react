b065c009662c6167c9ae2f67d26cd1524b567ef7
Shiyu Wang <shiyu3169@gmail.com>
2018-10-05 12:50:59 -0400

    fix typo

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ require("./server/app")(app);
 // For Build: Catch all other routes and return the index file -- BUILDING
 if (process.env.NODE_ENV === "production") {
     // Set static folder
-    app.use(espress.static("client/build"));
+    app.use(express.static("client/build"));
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
